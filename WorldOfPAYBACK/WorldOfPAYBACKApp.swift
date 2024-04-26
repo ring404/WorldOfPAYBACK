a611a442352d44d043e2c21741c424b5ee5c132b
Dimitreee <christoph.florian@gmail.com>
2024-04-26 12:09:04 +0200

    Initial Commit

---
+++ WorldOfPAYBACK/WorldOfPAYBACKApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPAYBACKApp.swift
+//  WorldOfPAYBACK
+//
+//  Created by Dmitrii on 26.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPAYBACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
